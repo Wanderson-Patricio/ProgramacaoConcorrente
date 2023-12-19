1179a839a6da619767773945237584189fd68ad6
Wanderson-Patricio <118014817+Wanderson-Patricio@users.noreply.github.com>
2023-12-19 09:33:55 -0300

    Update pi_approximation.c

--- Exercicio03/pi_approximation.c
+++ Exercicio03/pi_approximation.c
@@ -4,7 +4,7 @@
 #include <time.h>
 
 int main(){
-    int number_of_threads = 3;
+    int number_of_threads = 1;
     int N = 1000000000;
     double x, deltaX, sum = 0.0, pi_serial, pi_parallel;
 
@@ -13,9 +13,9 @@ int main(){
 
     //Região Sequencial
     start = omp_get_wtime();
-    for(int i=0; i<=N; i++){
+    for(int i=0; i<N; i++){
         x = (i+0.5)*deltaX;
-        sum += (4/(1+x*x));
+        sum += (4.0/(1.0+x*x));
     }
     end = omp_get_wtime();
     pi_serial = sum * deltaX;
@@ -27,9 +27,9 @@ int main(){
     #pragma omp parallel num_threads(number_of_threads) private(x) 
     {
         #pragma omp for reduction(+: sum)
-        for(int i=0; i<=N; i++){
+        for(int i=0; i<N; i++){
             x = (i+0.5)*deltaX;
-            sum += (4/(1+x*x));
+            sum += 4.0/(1.0+x*x);
         }
     }
     end = omp_get_wtime();
@@ -39,12 +39,12 @@ int main(){
     double speedup = serial_time/parallel_time;
     double efficiency = speedup/number_of_threads;
 
-    printf("pi serial = \t%.10f \n", pi_serial);
-    printf("pi parallel = \t%.10f \n", pi_parallel);
-    printf("serial time = \t%.5f s\n", serial_time);
-    printf("parallel time = %.5f s\n", parallel_time);
-    printf("Speedup = \t%.5f \n", speedup);
-    printf("Efficiency = \t%.5f \n", efficiency);
+    printf("pi serial \t= %.15f \n", pi_serial);
+    printf("pi parallel \t= %.15f \n", pi_parallel);
+    printf("serial time \t= %.5f s\n", serial_time);
+    printf("parallel time   = %.5f s\n", parallel_time);
+    printf("Speedup \t= %.5f \n", speedup);
+    printf("Efficiency \t= %.5f \n", efficiency);
 
     return 0;
 }
